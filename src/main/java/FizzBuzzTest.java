adb3ae988d9cb6113bebb184996c567e092e537e
Domenis Nicola S262688 <nicoladomenis23@hotmail.it>
2020-11-13 19:55:56 +0100

    first commit

---
+++ src/main/java/FizzBuzzTest.java
@@ -0,0 +1,23 @@
+public class FizzBuzzTest {
+
+    public static void main(String[] args) {
+        for (int i = 1 ; i<106;i++){
+            if(i%5==0 && i%3==0 && i%7==0)
+                System.out.println("FizzBuzzBang");
+            else if (i%5==0 && i%3==0)
+                System.out.println("FizzBuzz");
+            else if (i%7==0 && i%3==0)
+                System.out.println("FizzBang");
+            else if (i%7==0 && i%5==0)
+                System.out.println("BuzzBang");
+            else if (i%5==0)
+                System.out.println("Buzz");
+            else if (i%3==0)
+                System.out.println("Fizz");
+            else if (i%7==0)
+                System.out.println("Bang");
+            else
+                System.out.println(i);
+        }
+    }
+}
